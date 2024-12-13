fc3bf7aa51d06061a75d2453a7d3f58568776ca3
Tauno Tamm. <tauno171@gmail.com>
2024-12-14 00:33:29 +0200

    add week 5 code

---
+++ Week5LogicalOperators/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+//AND - &&
+
+//true && true --> true
+//true && flase --> false
+//false && true --> false
+//false && fakse --> false
+
+//OR - || (pipes)
+
+//true || true --> true
+//true || flase --> true
+//false || true --> true
+//false || fakse --> false
